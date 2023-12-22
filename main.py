4a47dc9677cf31a1a08f534904593d08838e15f3
AlisaShapovalova111 <130835724+AlisaShapovalova111@users.noreply.github.com>
2023-12-22 15:25:40 +0300

    add if

--- main.py
+++ main.py
@@ -8,3 +8,11 @@ reg = r'[A-ZА-Я]'
 
 # Найти все прописные буквы в строке с помощью регулярного выражения
 uppercase_letters = re.findall(reg, input_string)
+
+# Вывести найденные прописные буквы
+if uppercase_letters:
+    print("Прописные буквы в строке:")
+    for letter in uppercase_letters:
+        print(letter)
+else:
+    print("Прописных букв не найдено.")
